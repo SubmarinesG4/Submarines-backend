41c8e7a719a122dffc4078935f867f95703d7427
cescmatteo <matteo.cescon@studenti.unipd.it>
2023-05-25 15:02:50 +0200

    sistemati i filtri + qualche controllo

--- src/functions/translations/getAll/handler.ts
+++ src/functions/translations/getAll/handler.ts
@@ -4,19 +4,34 @@ import { authorizer } from "src/middleware/validators";
 import { DynamoDBHandler } from "src/services/dynamoDBHandler";
 
 const translationGetAll = async (event) => {
+
 	const tenantId: string = event.pathParameters.tenantId as string;
-
 	const dynamo = DynamoDBHandler.getInstance();
+	const params = {
+		date: event.queryStringParameters.date,
+		published: event.queryStringParameters.published,
+		word: event.queryStringParameters.word
+	}
 
-	const queryStringParameters = event.queryStringParameters;
+	//* Controllo che published sia true o false
+	if (params.published != undefined && params.published != "true" && params.published != "false")
+		return formatJSONResponse(
+			{ error: "published must be true or false" }, 400
+		);
+
+	//* Controllo che date sia una data valida
+	if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(params.date))
+		return formatJSONResponse(
+			{ error: "date must be a valid date" }, 400
+		);
 	
 	try {
 		var translations: any;
-		if(queryStringParameters == null || ( queryStringParameters.published == "" || queryStringParameters.date == "" || queryStringParameters.word == "")){
+		if(params.date != undefined || params.published != undefined || params.word != undefined){
+			translations = await dynamo.getScannedTranslations("TRAD#" + tenantId, params);
+			console.log("Filtrate");
+		} else {
 			translations = await dynamo.getAllTranslations("TRAD#" + tenantId);
-		}
-		else {
-			translations = await dynamo.getScannedTranslations("TRAD#" + tenantId, queryStringParameters);	
 		}
 
 		return formatJSONResponse(
